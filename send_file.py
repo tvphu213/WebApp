1a82b4a18743076f4022ad44e0c3b73205e69737
Trinh Vinh Phu <tvphu213@gmail.com>
2021-12-01 11:12:52 +0700

    up

--- send_file.py
+++ send_file.py
@@ -5,31 +5,12 @@ import boto3
 app = Flask(__name__)
 
 
-# create an STS client object that represents a live connection to the
-# STS service
-sts_client = boto3.client('sts')
-
-# Call the assume_role method of the STSConnection object and pass the role
-# ARN and a role session name.
-# assumed_role_object = sts_client.assume_role(
-#     RoleArn="arn:aws:iam::424562183293:role/Ec2AccessToS3",
-#     RoleSessionName="AssumeRoleSession1"
-# )
-
-# From the response that contains the assumed role, get the temporary
-# credentials that can be used to make subsequent API calls
-# credentials = assumed_role_object['Credentials']
-
 # Use the temporary credentials that AssumeRole returns to make a
 # connection to Amazon S3
 s3_resource = boto3.resource('s3')
-# aws_access_key_id=credentials['AccessKeyId'],
-# aws_secret_access_key=credentials['SecretAccessKey'],
-# aws_session_token=credentials['SessionToken'],
 
 
 BUCKET_NAME = 'phutv12-final-audit-bucket'
-
 
 @app.route('/')
 def home():
